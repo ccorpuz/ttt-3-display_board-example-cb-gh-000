9a7800366e3e51efdbcedbfa7e69ceca5ee53fce
Chester Corpuz <chestercorpuzz@gmail.com>
2018-06-21 08:32:20 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,6 +1,6 @@
 # Define a method display_board that prints a 3x3 Tic Tac Toe Board
 spaces = [" "," "," "," "," "," "," "," "," ",]
-def display_board:
+def display_board
   print(spaces[0] + "|"  + spaces[1] + "|" + spaces[2])
   puts(spaces[3] + "|" + spaces[4] + "|" + spaces[5])
   puts(spaces[6] + "|" + spaces[7] + "|" + spaces[8])

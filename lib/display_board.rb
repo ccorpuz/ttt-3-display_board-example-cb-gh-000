4e532a706bbbb3a089afe465cc6f731e6c928406
Chester Corpuz <chestercorpuzz@gmail.com>
2018-06-21 08:31:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -4,3 +4,5 @@ def display_board:
   puts(spaces[0] + "|"  + spaces[1] + "|" + spaces[2])
   puts(spaces[3] + "|" + spaces[4] + "|" + spaces[5])
   puts(spaces[6] + "|" + spaces[7] + "|" + spaces[8])
+
+display_board()
